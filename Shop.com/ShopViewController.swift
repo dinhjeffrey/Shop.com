41091f0c366efdcceadc40e7b18f31951feb9ae9
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-18 13:39:48 -0700

    added products api call

--- Shop.com/ShopViewController.swift
+++ Shop.com/ShopViewController.swift
@@ -10,28 +10,33 @@ import UIKit
 import Alamofire
 
 class ShopViewController: UIViewController {
-// API Docs: http://developer.shop.com/documentation
+    // API Docs: http://developer.shop.com/documentation
     
+    //    //    Method to encode parameters to json
+    //        func encodeJson(url: String, params: [String: AnyObject]) -> [String: AnyObject] {
+    //            var request = NSMutableURLRequest(URL: NSURL(fileURLWithPath: url))
+    //            let encoding = Alamofire.ParameterEncoding.URL
+    //            (request, _) = encoding.encode(request, parameters: params)
+    //            return params
+    //        }
     
-//    //    Method to encode parameters to json
-//        func encodeJson(url: String, params: [String: AnyObject]) -> [String: AnyObject] {
-//            var request = NSMutableURLRequest(URL: NSURL(fileURLWithPath: url))
-//            let encoding = Alamofire.ParameterEncoding.URL
-//            (request, _) = encoding.encode(request, parameters: params)
-//            return params
-//        }
-//        
-
-    
-    @IBAction func test() {
-        
-        let url = "https://api.shop.com/AffiliatePublisherNetwork/v1/categories"
-        let params = [
-            "publisherID": "TEST",
-            "locale": "en_US",
+    var url = "https://api.shop.com/AffiliatePublisherNetwork/v1/"
+    var params = [
+        "publisherID": "TEST",
+        "locale": "en_US",
         ]
-        //let encodedParams = encodeJson(url, params: params)
-        let headers = ["apikey": "l7xxe5c08ba7f05d41d3b8ee3bbb481d30d5"]
+    let headers = ["apikey": "l7xxe5c08ba7f05d41d3b8ee3bbb481d30d5"]
+    
+    @IBAction func categoriesPressed() {
+        categories()
+    }
+    @IBAction func productsPressed() {
+        products()
+    }
+    
+    
+    private func categories() {
+        url += "categories"
         
         Alamofire.request(
             .GET,
@@ -40,41 +45,66 @@ class ShopViewController: UIViewController {
             headers: headers
             )
             .responseJSON { response in
-//                print(response.request)  // original URL request
-//                print(response.response) // URL response
-//                print(response.data)     // server data
                 print(response.result)   // result of response serialization
                 
                 if let JSON = response.result.value {
                     print("JSON: \(JSON)")
                 }
         }
-
+    }
+    private func products() {
+        url += "products"
+        
+        let params = [
+            "publisherID": "TEST",
+            "locale": "en_US",
+            "start": "",
+            "perPage": "",
+            "term": "",
+            "categoryId": "",
+            "brandId": "Vans",
+            "sellerId": "",
+            "priceRangeId": ""
+        ]
+        
+        Alamofire.request(
+            .GET,
+            url,
+            parameters: params,
+            headers: headers
+            )
+            .responseJSON { response in
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
     }
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
