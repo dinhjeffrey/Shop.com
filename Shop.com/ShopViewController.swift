a8fa49c67229d4d15e9c1ae103600e7883f2778f
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-18 11:36:42 -0700

    working api call

--- Shop.com/ShopViewController.swift
+++ Shop.com/ShopViewController.swift
@@ -13,5 +13,68 @@ class ShopViewController: UIViewController {
 // API Docs: http://developer.shop.com/documentation
     
     
+//    //    Method to encode parameters to json
+//        func encodeJson(url: String, params: [String: AnyObject]) -> [String: AnyObject] {
+//            var request = NSMutableURLRequest(URL: NSURL(fileURLWithPath: url))
+//            let encoding = Alamofire.ParameterEncoding.URL
+//            (request, _) = encoding.encode(request, parameters: params)
+//            return params
+//        }
+//        
+
+    
+    @IBAction func test() {
+        
+        let url = "https://api.shop.com/AffiliatePublisherNetwork/v1/categories"
+        let params = [
+            "publisherID": "TEST",
+            "locale": "en_US",
+        ]
+        //let encodedParams = encodeJson(url, params: params)
+        let headers = ["apikey": "l7xxe5c08ba7f05d41d3b8ee3bbb481d30d5"]
+        
+        Alamofire.request(
+            .GET,
+            url,
+            parameters: params,
+            headers: headers
+            )
+            .responseJSON { response in
+//                print(response.request)  // original URL request
+//                print(response.response) // URL response
+//                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
